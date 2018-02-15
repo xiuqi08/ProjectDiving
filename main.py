bebf4015dff0d8537778f4fbd974fa782cf40739
petrelli08 <xiuqi08@gmail.com>
2018-02-15 13:13:25 +0900

    Showing price rather than sport

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from flask import request
 app = flask.Flask(__name__)
 
 @app.route('/')
-def index(sport=0):
-    return render_template('index.html',selection = ["Snorkelling","Diving"], sport = sport)
+def index(sport=0,price=0):
+    return render_template('index.html',selection = ["Snorkelling","Diving"], sport = sport, price=price)
 
 @app.route('/price', methods = ['GET','POST'])
-def price(sport=0):
+def price(sport=0,price=0):
     sport = request.form['sports']
-    return render_template('index.html',selection = ["Snorkelling","Diving"], sport = sport)
+    calculator = PriceCalculating()
+    calculator.setSport(sport)
+    price = calculator.calculate()
+    return render_template('index.html',selection = ["Snorkelling","Diving"], sport = sport, price = price)
 
-class PrizeCalculating():
+class PriceCalculating():
     #Initialize as 0, 
     __sport = 0
     
